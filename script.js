b364c9ba4c4c71fc60ce5b71180f1005189e5260
Aninha <103537945+aninhaelisa@users.noreply.github.com>
2024-05-10 18:12:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const arrayHeight = document.getElementsByClassName('height-nav');
+const navHeight = document.getElementsByTagName('nav')[0].clientHeight;
+
+for (let navHeightObj of arrayHeight)
+    navHeightObj.style.height = navHeight + 'px';
+
+function abrirMenu() {
+    const botoesMenu = document.getElementById('botoesMenu');
+
+    botoesMenu.className = botoesMenu.className.includes('responsivo') ? '' : 'responsivo'
+}
